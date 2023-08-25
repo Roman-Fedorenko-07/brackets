d5b487177696d320c2d59f8420731a4aefc536a3
Roman-Fedorenko-07 <rmn.fedorenko7@gmail.com>
2023-08-25 15:37:23 +0300

    fix: files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(s, brackets) {
+  brackets = {
+    ')': '(',
+    ']': '[',
+    '}': '{'
+  };
+  const st = [];
+  for (let i = 0; i < s.length; i++) {
+    if (isClosedBracket(s[i])) {
+      if (brackets[s[i]] !== st.pop()) return false;
+    } else {
+      st.push(s[i]);
+    }
+  }
+  return st.length === 0;
+}
+function isClosedBracket(ch) {
+  return [')', ']', '}'].indexOf(ch) > -1;
 }
